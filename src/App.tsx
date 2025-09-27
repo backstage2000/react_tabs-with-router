2f986265bf18fe9f09aff7e9f823938bea4d024d
Anatoliy Korotenko <tolik.korotenko@21gmail.com>
2025-09-27 14:06:48 +0300

    sol2

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,6 @@ import './App.scss';
 import { Outlet, NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 
-// const tabs = [
-//   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
-//   { id: 'tab-2', title: 'Tab 2', content: 'Some text 2' },
-//   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
-// ];
-
 const getLinkClass = ({ isActive }: { isActive: boolean }) =>
   classNames('navbar-item', {
     'is-active': isActive,
@@ -37,27 +31,6 @@ export const App = () => (
 
     <div className="section">
       <div className="container">
-        {/* <h1 className="title">Home page</h1>
-        <h1 className="title">Tabs page</h1>
-        <h1 className="title">Page not found</h1>
-
-        <div className="tabs is-boxed">
-          <ul>
-            <li data-cy="Tab" className="is-active">
-              <a href="#/">Tab 1</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 2</a>
-            </li>
-            <li data-cy="Tab">
-              <a href="#/">Tab 3</a>
-            </li>
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          Please select a tab
-        </div> */}
         <Outlet />
       </div>
     </div>
--- src/Components/TabsList.tsx
+++ src/Components/TabsList.tsx
@@ -24,23 +24,6 @@ export const TabsList = () => {
               <Link to={`/tabs/${tab.id}`}>{tab.title}</Link>
             </li>
           ))}
-          {/* <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
-
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li> */}
         </ul>
       </div>
 
